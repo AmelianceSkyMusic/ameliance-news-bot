6a6b5b01efc5b4a5b03feb4696dd2d7b5ad90740
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-06-12 22:34:38 +0300

    refactor: update post interval function

--- src/actions/command/post-interval.ts
+++ src/actions/command/post-interval.ts
@@ -1,5 +1,4 @@
 import { bot } from '../../../api/bot';
-import { getRandomNumber } from '../../shared/_asm/scripts/get-random-number';
 import { handleAppError } from '../../shared/helpers/handle-app-error';
 import { hasNoAccess } from '../../shared/helpers/has-no-access';
 import { logUserInfo } from '../../shared/helpers/log-user-info';
@@ -7,21 +6,20 @@ import { replyError } from '../../shared/helpers/reply-error';
 import { runWithRandomInterval } from '../../shared/helpers/run-with-random-interval';
 import { sendNewsPost } from '../helpers/send-news-post';
 
-const oneMinInMs = 1000 * 60;
-const minIntervalInMs = oneMinInMs * 20; //* 30min
-const maxIntervalInMs = oneMinInMs * 60; //* 60min
-
 export function postInterval() {
    bot.command('postInterval', (ctx) => {
       try {
          logUserInfo(ctx, 'command post interval');
          if (hasNoAccess({ ctx })) return;
 
-         const randomInterval = getRandomNumber(minIntervalInMs, maxIntervalInMs);
-
-         runWithRandomInterval(async () => {
-            await sendNewsPost(ctx);
-         }, randomInterval);
+         runWithRandomInterval(
+            ctx,
+            async () => {
+               await sendNewsPost(ctx);
+            },
+            20,
+            40,
+         );
       } catch (error) {
          const { code, message } = handleAppError(error);
          replyError(ctx, { code, message });
--- src/shared/helpers/run-with-random-interval.ts
+++ src/shared/helpers/run-with-random-interval.ts
@@ -1,10 +1,24 @@
-export async function runWithRandomInterval(callback: () => void, randomInterval: number) {
+import { getCurrentTimeWithOffset } from '../_asm/scripts/get-current-time-with-offset';
+import { getRandomNumber } from '../_asm/scripts/get-random-number';
+
+import { Context } from 'grammy';
+
+const oneMinInMs = 1000 * 60;
+
+export async function runWithRandomInterval(
+   ctx: Context,
+   callback: () => void,
+   min: number,
+   max: number,
+) {
    callback();
 
    console.time('runWithRandomInterval');
+   const randomInterval = getRandomNumber(min, max) * oneMinInMs;
+   ctx.reply(`Next post will be sent at: ${getCurrentTimeWithOffset(randomInterval)}`);
 
    setTimeout(() => {
       console.timeEnd('runWithRandomInterval');
-      runWithRandomInterval(callback, randomInterval);
+      runWithRandomInterval(ctx, callback, min, max);
    }, randomInterval);
 }
