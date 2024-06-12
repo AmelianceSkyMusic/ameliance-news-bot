aca3eaf0c20fbdd0b77b46abf8af7cbb0e8b325e
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-06-12 16:37:42 +0300

    refactor: remove console

--- src/actions/on/message.ts
+++ src/actions/on/message.ts
@@ -14,17 +14,13 @@ export function message() {
          if (hasNoAccess({ ctx })) return;
 
          const text = ctx.msg.caption;
-         console.log('1 text: ', text);
          const photoUrl = ctx.msg.photo?.[0].file_id;
-         console.log('2 photoUrl: ', photoUrl);
          if (!photoUrl) return;
 
          const articleText = text?.split('\n').splice(1).join('\n').trim();
-         console.log('3 articleText: ', articleText);
          if (!articleText) return;
 
          const postAsHTML = await generateBimbaPostAsHTML(articleText, ctx);
-         console.log('10 postAsHTML: ', postAsHTML);
          if (!postAsHTML) return;
 
          await ctx.api.sendPhoto(Number(process.env.BIMBA_NEWS_ID), photoUrl, {
@@ -32,6 +28,10 @@ export function message() {
             parse_mode: 'HTML',
          });
 
+         await ctx.replyWithPhoto(photoUrl, {
+            caption: postAsHTML,
+            parse_mode: 'HTML',
+         });
          console.timeEnd('on message');
       } catch (error) {
          const { code, message } = handleAppError(error);
