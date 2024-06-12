ca699537122b71cdb6bc856eeed8d2540f98d63e
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-06-12 12:17:28 +0300

    refactor: add post status notifications

--- src/actions/helpers/generate-bimba-post-as-html.ts
+++ src/actions/helpers/generate-bimba-post-as-html.ts
@@ -39,11 +39,15 @@ export async function generateBimbaPostAsHTML(text: string, ctx: Context) {
       const prompt = generatePrompt({ text });
       ctx.reply('...sent a Gemini prompt...');
       let geminiAnswer = await sendPromptGemini(prompt);
+      ctx.reply(`...received a response with the number of characters: ${geminiAnswer.length}...`);
 
       //* If the answer exceeds the character limit, try again or return when the limit is exceeded again
       if (geminiAnswer.length > 1000) {
          ctx.reply('...character limit exceeded\nSent a second Gemini prompt...');
          geminiAnswer = await sendPromptGemini(prompt);
+         ctx.reply(
+            `...received a response with the number of characters: ${geminiAnswer.length}...`,
+         );
       }
 
       if (geminiAnswer.length > 1000) {
