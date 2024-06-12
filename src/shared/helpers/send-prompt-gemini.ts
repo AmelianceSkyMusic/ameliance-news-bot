6d6dbb12f60de665fb9d528003812310ab64b749
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-06-12 12:32:33 +0300

    chore: add test console.log

--- src/shared/helpers/send-prompt-gemini.ts
+++ src/shared/helpers/send-prompt-gemini.ts
@@ -11,9 +11,14 @@ export async function sendPromptGemini(prompt: string, ctx: Context) {
    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
    ctx.reply('3');
    const result = await model.generateContent(prompt);
+   console.log('result: ', result);
    ctx.reply('2');
-   const text = result.response.text();
-   console.log('text: ', text);
+   const response = await result.response;
+   console.log('response: ', response);
    ctx.reply('1');
+   const text = response.text();
+
+   console.log('text: ', text);
+   ctx.reply('0');
    return text;
 }
