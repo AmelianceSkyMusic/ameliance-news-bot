b424a1b71503b92c3423146fb139706836f4165f
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-11-07 13:20:39 +0200

    chore: add console.time

--- src/shared/helpers/run-with-timeout.ts
+++ src/shared/helpers/run-with-timeout.ts
@@ -13,8 +13,13 @@ export async function runWithTimeout(
    statusMessage?: string,
    timeoutMs: number = 9000,
 ) {
+   console.time('1');
+   console.time('2');
+   console.time('3');
+   console.time('4');
    const timeout = new Promise<void>((resolve) =>
       setTimeout(async () => {
+         console.time('2');
          console.warn(`${APP.name} > Operation timed out`);
          const warnMessageResp = await replyHTML(ctx, 'Сарян, братан, я обідаю...');
          await removeMessageById({ ctx, messageId: warnMessageResp.message_id });
@@ -28,10 +33,13 @@ export async function runWithTimeout(
       console.log('Starting callback execution...');
       await Promise.race([callback(ctx), timeout]);
       console.log('Callback execution completed successfully.');
+      console.time('3');
    } catch (error) {
       console.error('Error in callback:', error);
       handleAppError(error);
+      console.time('4');
    } finally {
       if (replyResp) await removeMessageById({ ctx, messageId: replyResp.message_id });
+      console.timeEnd('1');
    }
 }
